a275d1eae1838024f1d6625a30ddd5c081b19ecb
christianpalomo <70193525+christianpalomo@users.noreply.github.com>
2024-04-21 21:42:45 -0700

    Play sound effect when profile pic is clicked. Created sounds folder.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Regular JavaScript
+
+// play sound effect when profile pic is clicked
+document.querySelector(".profile-pic").addEventListener("click", function () {
+  var audio = new Audio("sounds/crash.mp3");
+  audio.play();
+});
+
+// jQuery
+
+// changes color of name on click
 $("p.name").on("click", function () {
   $("p.name").css("color", "#9a29b9");
 });
